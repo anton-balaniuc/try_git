17658d76a3b905e3a8c78e4630c20aff0fca7f4b
Anton Balaniuc <alagar86@gmail.com>
2012-12-22 12:11:55 +0200

    setter +new  variable

--- SimpleClass.class.php
+++ SimpleClass.class.php
@@ -1,7 +1,11 @@
 <?php 
 class SimpleClass{
 	private $field = null;
+	private $foo = 10;
 	public function getField(){
 		return $this->field;
+	}
+	public function setField($field){
+		$this->field = $field;
 	}
 }
